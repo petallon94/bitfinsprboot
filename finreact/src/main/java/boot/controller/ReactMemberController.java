d1cfd5ee73f625c0d11b417f3c8402aec25b636e
petallon94 <petallon94@naver.com>
2021-02-09 00:00:30 +0900

    member 구현(back)

--- finreact/src/main/java/boot/controller/ReactMemberController.java
+++ finreact/src/main/java/boot/controller/ReactMemberController.java
@@ -1,11 +1,13 @@
 package boot.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,14 +27,40 @@ public class ReactMemberController {
 	@PostMapping("/member/add")
 	public void dataAdd(@RequestBody MemberDto mdto)
 	{
-		System.out.println("react 출력 시작");
-		System.out.println("memailid="+mdto.getMemailid());
-		System.out.println("menick="+mdto.getMnick());//toString 자동호출
-		System.out.println("mema="+mdto.getMpw());
+		//System.out.println("react 출력 시작");
+		//System.out.println("memailid="+mdto.getMemailid());
+		//System.out.println("menick="+mdto.getMnick());//toString 자동호출
+		//System.out.println("mema="+mdto.getMpw());
 		//memberdto 에 추가
 		mapper.insertMem(mdto);
 		
 	}
 	
+	@PostMapping("/member/checkEmail")
+	public int checkEmail(@RequestBody MemberDto mdto) {
+		String memailid = mdto.getMemailid();
+		int ce = mapper.checkEmail(memailid);
+		System.out.println("memeilid : "+memailid + "   ce : "+ ce);
+	
+		return ce;
+	}
+	
+	@PostMapping("/member/checkNick")
+	public int checkNick(@RequestBody MemberDto mdto) {
+		String mnick = mdto.getMnick();
+		int ce = mapper.checkNick(mnick);
+		System.out.println("meNICK : "+ mnick + "   ce : "+ ce);
+	
+		return ce;
+	}
+	
+	@PostMapping("/member/memCheck")
+	public MemberDto memberCheck(@RequestBody MemberDto mdto){
+		
+		System.out.println(mdto.getMemailid()+" "+mdto.getMpw());
+		MemberDto mlist = mapper.LoginCheck(mdto);
+		System.out.println(mlist);
+		return mlist;
+	}
 
 }
--- finreact/src/main/java/boot/dao/MysqlMemberMapper.java
+++ finreact/src/main/java/boot/dao/MysqlMemberMapper.java
@@ -12,5 +12,7 @@ public interface MysqlMemberMapper {
 	public List<MemberDto> getData(String mnum);
 	public List<MemberDto> getFollowing(String mnum);
 	public void insertMem(MemberDto mdto);
-
+	public int checkEmail(String memailid);
+	public int checkNick(String mnick);
+	public MemberDto LoginCheck(MemberDto mdto);
 }
