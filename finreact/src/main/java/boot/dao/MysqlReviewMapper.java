cae6cbda1fc923951aa99e0c26863f92ffcb97f1
DESKTOP-R6MAOKT\최수빈 <soobin9257@naver.com>
2021-02-13 22:55:14 +0900

    커밋합니다

--- finreact/src/main/java/boot/dao/MysqlReviewMapper.java
+++ finreact/src/main/java/boot/dao/MysqlReviewMapper.java
@@ -9,8 +9,21 @@ import boot.dto.ReviewDto;
 @Mapper
 public interface MysqlReviewMapper {
 	
-	public int getTotalCount();
-	//public List<ReviewDto> getAllDatas(int mnum);
+//	public int getTotalCount();
+//	//public List<ReviewDto> getAllDatas(int mnum);
 	public List<ReviewDto> getAllDatas();
 	public ReviewDto getData(String rnum);
+	
+	
+	public int getTotalCount();
+	//public List<ReviewDto> getAllDatas(String rmnum);
+	
+	public void insertReview (ReviewDto dto);
+	public void insertRestaurant (ReviewDto dto);
+	public void insertRepic (ReviewDto dto);
+	public void insertHashtag (ReviewDto dto);
+	
+	public int getRnumber(String rmnum);
+	
+	public ReviewDto getContent(int rnum);
 }
--- finreact/src/main/java/boot/dto/ReviewDto.java
+++ finreact/src/main/java/boot/dto/ReviewDto.java
@@ -12,150 +12,133 @@ import lombok.Data;
 public class ReviewDto {
 	
 	//review
-	private int rnum;
-	private int rmnum;
-	private String rcontent;
-	private int rscore;
-	private Timestamp rwriteday;
-	
-    //member
-    private int mnum;
-    private String memailid;
-    private String mnick;
-    private String mpw;
-	
-	//restaurant;
-	private int resnum;
-	private int revnum;
-	private String resname;
-	private String resaddr;
-	
-	//hashtag
-	private int crnum;
-	private String hashtag;
-	
-	//repic
-	private int pirnum;
-	private String picname;
-	
-	//likere
-	private int lmnum;
-	private int lrnum;
-	
-	public int getRnum() {
-		return rnum;
-	}
-	public void setRnum(int rnum) {
-		this.rnum = rnum;
-	}
-	public int getRmnum() {
-		return rmnum;
-	}
-	public void setRmnum(int rmnum) {
-		this.rmnum = rmnum;
-	}
-	public String getRcontent() {
-		return rcontent;
-	}
-	public void setRcontent(String rcontent) {
-		this.rcontent = rcontent;
-	}
-	public int getRscore() {
-		return rscore;
-	}
-	public void setRscore(int rscore) {
-		this.rscore = rscore;
-	}
-	public Timestamp getRwriteday() {
-		return rwriteday;
-	}
-	public void setRwriteday(Timestamp rwriteday) {
-		this.rwriteday = rwriteday;
-	}
-	public int getMnum() {
-		return mnum;
-	}
-	public void setMnum(int mnum) {
-		this.mnum = mnum;
-	}
-	public String getMemailid() {
-		return memailid;
-	}
-	public void setMemailid(String memailid) {
-		this.memailid = memailid;
-	}
-	public String getMnick() {
-		return mnick;
-	}
-	public void setMnick(String mnick) {
-		this.mnick = mnick;
-	}
-	public String getMpw() {
-		return mpw;
-	}
-	public void setMpw(String mpw) {
-		this.mpw = mpw;
-	}
-	public int getResnum() {
-		return resnum;
-	}
-	public void setResnum(int resnum) {
-		this.resnum = resnum;
-	}
-	public int getRevnum() {
-		return revnum;
-	}
-	public void setRevnum(int revnum) {
-		this.revnum = revnum;
-	}
-	public String getResname() {
-		return resname;
-	}
-	public void setResname(String resname) {
-		this.resname = resname;
-	}
-	public String getResaddr() {
-		return resaddr;
-	}
-	public void setResaddr(String resaddr) {
-		this.resaddr = resaddr;
-	}
-	public int getCrnum() {
-		return crnum;
-	}
-	public void setCrnum(int crnum) {
-		this.crnum = crnum;
-	}
-	public String getHashtag() {
-		return hashtag;
-	}
-	public void setHashtag(String hashtag) {
-		this.hashtag = hashtag;
-	}
-	public int getPirnum() {
-		return pirnum;
-	}
-	public void setPirnum(int pirnum) {
-		this.pirnum = pirnum;
-	}
-	public String getPicname() {
-		return picname;
-	}
-	public void setPicname(String picname) {
-		this.picname = picname;
-	}
-	public int getLmnum() {
-		return lmnum;
-	}
-	public void setLmnum(int lmnum) {
-		this.lmnum = lmnum;
-	}
-	public int getLrnum() {
-		return lrnum;
-	}
-	public void setLrnum(int lrnum) {
-		this.lrnum = lrnum;
-	}
+		private int rnum;
+		private int rmnum;
+		private String rcontent;
+		private int rscore;
+		@JsonFormat(pattern = "yyyy-MM-dd",timezone = "Asia/Seoul")
+		private Timestamp rwriteday;
+		private String rcategory;
+		
+		//restaurant;
+		private int resnum;
+		private int revnum;
+		private String resname;
+		private String resaddr;
+		
+		//hashtag
+		private int crnum;
+		private String hashtag;
+		
+		//repic
+		private int pirnum;
+		private String picname;
+		
+		//likere
+		private int lmnum;
+		private int lrnum;
+		
+		
+		
+		public int getRnum() {
+			return rnum;
+		}
+		public void setRnum(int rnum) {
+			this.rnum = rnum;
+		}
+		public int getRmnum() {
+			return rmnum;
+		}
+		public void setRmnum(int rmnum) {
+			this.rmnum = rmnum;
+		}
+		public String getRcontent() {
+			return rcontent;
+		}
+		public void setRcontent(String rcontent) {
+			this.rcontent = rcontent;
+		}
+		public int getRscore() {
+			return rscore;
+		}
+		public void setRscore(int rscore) {
+			this.rscore = rscore;
+		}
+		public Timestamp getRwriteday() {
+			return rwriteday;
+		}
+		public void setRwriteday(Timestamp rwriteday) {
+			this.rwriteday = rwriteday;
+		}
+		public String getRcategory() {
+			return rcategory;
+		}
+		public void setRcategory(String rcategory) {
+			this.rcategory = rcategory;
+		}
+		public int getResnum() {
+			return resnum;
+		}
+		public void setResnum(int resnum) {
+			this.resnum = resnum;
+		}
+		public int getRevnum() {
+			return revnum;
+		}
+		public void setRevnum(int revnum) {
+			this.revnum = revnum;
+		}
+		public String getResname() {
+			return resname;
+		}
+		public void setResname(String resname) {
+			this.resname = resname;
+		}
+		public String getResaddr() {
+			return resaddr;
+		}
+		public void setResaddr(String resaddr) {
+			this.resaddr = resaddr;
+		}
+		public int getCrnum() {
+			return crnum;
+		}
+		public void setCrnum(int crnum) {
+			this.crnum = crnum;
+		}
+		
+		public int getPirnum() {
+			return pirnum;
+		}
+		public void setPirnum(int pirnum) {
+			this.pirnum = pirnum;
+		}
+		public String getPicname() {
+			return picname;
+		}
+		public void setPicname(String picname) {
+			this.picname = picname;
+		}
+		public int getLmnum() {
+			return lmnum;
+		}
+		public void setLmnum(int lmnum) {
+			this.lmnum = lmnum;
+		}
+		public int getLrnum() {
+			return lrnum;
+		}
+		public void setLrnum(int lrnum) {
+			this.lrnum = lrnum;
+		}
+		public String getHashtag() {
+			return hashtag;
+		}
+		public void setHashtag(String hashtag) {
+			this.hashtag = hashtag;
+		}
+		
+
 	
 	
 
