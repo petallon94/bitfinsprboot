be00a18136908058fb5cf4d7e7ed470a0579c9d5
DESKTOP-Q8PHHQK\Leesangheon <yahoo9886@kakao.com>
2021-02-13 11:43:57 +0900

    dsdsdsd

--- finreact/src/main/java/boot/controller/ReactReviewController.java
+++ finreact/src/main/java/boot/controller/ReactReviewController.java
@@ -29,12 +29,10 @@ public class ReactReviewController {
 	{		
 		return mapper.getAllDatas();
 	}
-<<<<<<< HEAD
 
-=======
 	@GetMapping("/review/detail")
 	public ReviewDto getData(@RequestParam String rnum) {
 		return mapper.getData(rnum);
 	}
->>>>>>> 3do
+
 }
